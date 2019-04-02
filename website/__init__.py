dd4bea948f76500713975f3f70869c24f188f748
Kyle Williams <kjw22@buffalo.edu>
2019-04-02 19:57:10 -0400

    login functionality with username validation

--- website/__init__.py
+++ website/__init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 
 application = Flask(__name__)
+application.config.from_pyfile('config.py')
+application.config['SECRET_KEY']
 
 from website import routes
--- website/routes.py
+++ website/routes.py
@@ -1,7 +1,13 @@
 from website import application
 from flask import render_template
+from website.forms import LoginForm
 
 @application.route('/')
 @application.route('/home')
 def home():
     return render_template('index.html', title="Welcome!")
+
+@application.route('/login')
+def login():
+    form = LoginForm()
+    return render_template('login.html', title="Log In", form=form)
