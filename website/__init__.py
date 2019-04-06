2142e41ad6e350358f4b8f3322fd6ebde4cf6153
KyleWilliamz <kjw22@buffalo.edu>
2019-04-06 16:18:55 -0400

    log in functionality

--- website/__init__.py
+++ website/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 application = Flask(__name__)
 application.config.from_pyfile('config.py')
@@ -8,5 +9,8 @@ application.config['SECRET_KEY']
 application.config['SQLALCHEMY_DATABASE_URI']
 db = SQLAlchemy(application)
 migrate = Migrate(application, db)
+login = LoginManager(application)
+login.login_view = 'login'
+login.login_message_category= 'info'
 
 from website import routes, models
--- website/routes.py
+++ website/routes.py
@@ -1,6 +1,10 @@
 from website import application
-from flask import render_template
+from flask import render_template, redirect, request, flash
 from website.forms import LoginForm
+from flask_login import current_user, login_user, login_required, logout_user
+from website.models import User
+from werkzeug.urls import url_parse
+
 
 @application.route('/')
 @application.route('/home')
@@ -9,5 +13,31 @@ def home():
 
 @application.route('/login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('account'))
     form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
     return render_template('login.html', title="Log In", form=form)
+
+@application.route('/register')
+def register():
+    return render_template('register.html', title="Register!")
+
+@application.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@application.route('/account', methods=['GET', 'POST'])
+@login_required
+def account():
+    return render_template('account.html', title="Account")
